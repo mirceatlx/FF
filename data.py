7110db7acb3dbf7a875a3249c5c1c29e3db78c7c
David Dinucu-Jianu <D.dinucu-jianu@student.tudelft.nl>
2023-05-21 20:52:08 +0200

    Cleaned up Cifar, data and removed random py scripts

--- data.py
+++ data.py
@@ -2,60 +2,7 @@ import random
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-# pip install opensimplex
-from torch.utils.data import DataLoader, Dataset
-
-class CIFAR10:
-
-    @staticmethod
-    def overlay_y_on_x(x: torch.Tensor, y: torch.Tensor):
-
-        # torch.view acts as a pointer 
-        _data = x.clone()
-        # pad the image with zeros for the label
-        _data = _data.view(-1, _data.numel() // _data.shape[0]) # flatten the image
-        overlay = torch.zeros(_data.shape[0], 10)
-        overlay[range(_data.shape[0]), y] = x.max()
-        _data[:, :10] = overlay 
-        return _data, y
-
-    @staticmethod
-    def predict(data_loader: torch.utils.data.DataLoader, model: nn.Module, device="cpu"):
-        model.eval()
-        predictions = []
-        real = []
-        for i, (x, y) in enumerate(data_loader):
-            goodness = []
-            for num in range(10):
-                over_x = CIFAR10.overlay_y_on_x(x, num)
-                over_x = over_x[0].to(device)
-                good = model(over_x)
-                goodness.append(good.detach().cpu().numpy())
-            goodness = np.array(goodness).T
-            predictions.extend(np.argmax(goodness,axis = 1))
-            real.extend(list(y.detach().cpu().numpy()))
-        return np.array(predictions), np.array(real)
-
-    @staticmethod
-    def predict_resnet(data_loader: torch.utils.data.DataLoader, model: nn.Module, resnet, device="cpu"):
-        model.eval()
-        predictions = []
-        real = []
-        for i, (x, y) in enumerate(data_loader):
-            goodness = []
-            for num in range(10):
-                over_x = CIFAR10.overlay_y_on_x(x, num)
-                over_x = over_x[0].reshape((-1, 3, 224, 224))
-                over_x = resnet(over_x.to(device))
-                over_x = over_x.to(device)
-                good = model(over_x)
-                goodness.append(good.detach().cpu().numpy())
-            goodness = np.array(goodness).T
-            predictions.extend(np.argmax(goodness,axis = 1))
-            real.extend(list(y.detach().cpu().numpy()))
-        return np.array(predictions), np.array(real)
-
+from torch.utils.data import Dataset
 
 class MNIST:
 
