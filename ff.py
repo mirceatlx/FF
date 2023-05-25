c4af8b35d2cc32aa839098288da40ef8219f3cb8
David Dinucu-Jianu <D.dinucu-jianu@student.tudelft.nl>
2023-05-25 06:57:35 +0200

    Removed cell outputs and wandb

--- ff.py
+++ ff.py
@@ -2,7 +2,6 @@ import torch
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
-import wandb
 
 # reference: https://keras.io/examples/vision/forwardforward/
 class FFLayer(nn.Module):
