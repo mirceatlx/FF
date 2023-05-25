72aa58f68a8869bfccab21c20a91ea608b54f610
David Dinucu-Jianu <D.dinucu-jianu@student.tudelft.nl>
2023-05-25 06:52:15 +0200

    Cleaned up everything and added cifar-10

--- data.py
+++ data.py
@@ -62,7 +62,6 @@ class MergedDataset(Dataset):
     def __init__(self, original_dataset, steps, negative_data_generator = MNIST.negative_data_generation):
         self.original_dataset = original_dataset
         self.num_images = len(original_dataset)
-        a = self.original_dataset[0][0][0]
         self.steps = steps
         self.negative_data_generator = negative_data_generator
 
@@ -72,9 +71,7 @@ class MergedDataset(Dataset):
         index2 = random.randint(0, self.num_images - 1)
         img1, _ = self.original_dataset[index1]
         img2, _ = self.original_dataset[index2]
-
-
-        return self.negative_data_generator(img1[0], img2[0], self.steps)
+        return self.negative_data_generator(img1, img2, self.steps)
 
     def __len__(self):
         # Return the number of pairs we can make from the original dataset
@@ -130,5 +127,57 @@ class FashionMNIST:
             
             goodness = np.array(goodness).T
             predictions.extend(np.argmax(goodness,axis = 1))
+            real.extend(list(y.detach().cpu().numpy()))
+        return np.array(predictions), np.array(real)
+
+class CIFAR10:
+
+    @staticmethod
+    def overlay_y_on_x(x: torch.Tensor, y: torch.Tensor):
+        _data = x.clone()
+        _data = _data.view(-1, _data.numel() // _data.shape[0]) # flatten the image
+        overlay = torch.zeros(_data.shape[0], 10)
+        overlay[range(_data.shape[0]), y] = x.max()
+        _data[:, :10] = overlay 
+        return _data, y    
+
+    @staticmethod
+    def negative_data_generation(a: torch.Tensor, b: torch.Tensor, blurring_steps: int = 3):
+        """
+        Create negative data that has very long correlations but very similar short range correlations. 
+        :a: an individual image from the dataset
+        :b: an individual image from the dataset
+        :blurring_steps: number of steps to apply a blurring filter on the image
+        """
+        mask = torch.rand_like(a) < 0.5
+        mask = mask.float()
+        filter = torch.tensor([[1/16, 1/8, 1/16], [1/8, 1/4, 1/8], [1/16, 1/8, 1/16]]).to(a.device)
+        filter = filter.view(1, 1, 3, 3).repeat(3, 1, 1, 1)  # Repeat for 3 color channels
+
+        for step in range(blurring_steps):
+            mask = torch.nn.functional.conv2d(mask, filter, padding=1, groups=3)  # Apply filter independently for each channel
+
+        # threshold the image at 0.5
+        mask = mask < 0.5
+        mask = mask.int()
+        reversed_mask = torch.bitwise_not(mask.bool()).int()
+        neg_data = torch.mul(mask, a) + torch.mul(reversed_mask, b)
+        return neg_data
+
+    @staticmethod
+    def predict(data_loader: torch.utils.data.DataLoader, model: nn.Module, device="cpu"):
+        model.eval()
+        predictions = []
+        real = []
+        for i, (x, y) in enumerate(data_loader):
+            goodness = []
+            for num in range(10):
+                over_x = CIFAR10.overlay_y_on_x(x, num)
+                over_x = over_x[0].to(device)
+                good = model(over_x)
+                goodness.append(good.detach().cpu().numpy())
+
+            goodness = np.array(goodness).T
+            predictions.extend(np.argmax(goodness, axis=1))
             real.extend(list(y.detach().cpu().numpy()))
         return np.array(predictions), np.array(real)
